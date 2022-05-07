f8f587f3389ea3fba52beb1ff497caa1454f1177
Gabriel-O-M-L <60969331+Gabriel-O-M-L@users.noreply.github.com>
2022-05-07 11:12:30 -0300

    corrigindo dilatação
    corrigindo problemas de dilatação

--- dil_comp.m
+++ dil_comp.m
@@ -3,6 +3,8 @@ function y_comp = dil_comp(x,horiz_scale,vert_scale,conv_type)
   [horiz_numerator,horiz_denominator] = rat(horiz_scale);
   [vert_numerator,vert_denominator] = rat(vert_scale);
   [L,C] = size(x);
+  x = x/255;
+
 
   if (horiz_denominator != 1)
     N = horiz_numerator;
@@ -17,18 +19,21 @@ function y_comp = dil_comp(x,horiz_scale,vert_scale,conv_type)
     count_x = 1
 
     for i = 1:L
-      Y(i,1:insert_zero+1:end - insert_zero) = x(i,1:end)
+      Y(i,1:insert_zero+1:end - insert_zero) = x(i,1:end);
       count_x = count_x+1;
     endfor
 
+    imshow(Y)
     count_x = 1;
 
-   [yconv_L,yconv_C] = size(conv(Y(1,1:C+(C*insert_zero)),w));
+   [yconv_L,yconv_C] = size(conv(Y(1,1:end),w));
     new_conv = zeros(L,yconv_C);
 
     for i = 1:L
-      new_conv(L,1:yconv_C) = conv(Y(L,1:C+(C*insert_zero)),w);
+      new_conv(i,1:end) = conv(Y(i,1:end),w);
     endfor
+
+    imshow(new_conv)
 
     N = horiz_denominator;
 
@@ -42,17 +47,18 @@ function y_comp = dil_comp(x,horiz_scale,vert_scale,conv_type)
     new_yconv = zeros(L,new_yconv_C);
 
     for i = 1:L
-      new_yconv(L,1:new_yconv_C) = conv(new_conv(L,1:yconv_C),w);
+      new_yconv(i,1:new_yconv_C) = conv(new_conv(i,1:yconv_C),w);
     endfor
+
+    imshow(new_yconv)
 
     [y_comp_L,y_comp_C] = size(new_yconv(1,1:N:new_yconv_C));
      y_comp = zeros(L,y_comp_C);
 
      for i = 1:L
-      y_comp(L,1:y_comp_C) = new_yconv(L,1:N:new_yconv_C);
+      y_comp(i,1:y_comp_C) = new_yconv(i,1:N:new_yconv_C);
      endfor
 
     endif
-    imshow(y_comp)
 
 endfunction
